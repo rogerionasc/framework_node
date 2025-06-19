7931669a2f601a5c594b4a9671998150ea256cbb
Rogério Nascimento <rogerionascimentosantos@gmail.com>
2025-06-19 19:54:37 -0300

    Excluindo pasta e arquivos não usados

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,12 +2,3 @@
 export { default as Home } from './home';
 export { default as Fornecedor } from './fornecedor';
 export { default as Produto } from './produto';
-
-// Componentes comuns (quando criados)
-// export { default as Header } from './common/Header';
-// export { default as Footer } from './common/Footer';
-// export { default as Loading } from './common/Loading';
-
-// Componentes de layout (quando criados)
-// export { default as Layout } from './layout/Layout';
-// export { default as Sidebar } from './layout/Sidebar';
